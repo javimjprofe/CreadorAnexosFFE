5fde3404b96feea2a4eabfb8b6f8252601c10e1c
Javi M J Profe <javimjprofe@gmail.com>
2026-03-24 12:50:03 +0100

    Comienzo del proyecto.

---
+++ src/main/java/javimjprofe/creadoranexosffe/CreadorAnexosFFE.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package javimjprofe.creadoranexosffe;
+
+/**
+ *
+ * @author javimjprofe
+ */
+public class CreadorAnexosFFE {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
